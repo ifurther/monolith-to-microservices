05a180e78cf9847c3efdb68e678f7f8bc04b8e28
ifurther <55025025+ifurther@users.noreply.github.com>
2021-06-03 21:58:42 +0800

    add different port

--- k8sdeployment.sh
+++ k8sdeployment.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-
+deployname=$1
+if [ $deployname == orders ]; then
+deployappport=8081
+elif [ $deployname ==  products ];then
+deployappport=8082
+else
+deployappport=8080
 kubectl create deployment $1 --image=gcr.io/${GOOGLE_CLOUD_PROJECT}/$1:$2
-kubectl expose deployment $1 --type=LoadBalancer --port 80 --target-port 8080
+kubectl expose deployment $1 --type=LoadBalancer --port 80 --target-port $deployappport
