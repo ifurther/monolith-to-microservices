caf7e3d3d282c2716e03c1b67d97f89fa135baa1
ifurther <55025025+ifurther@users.noreply.github.com>
2021-06-03 22:01:12 +0800

    add fi

--- k8sdeployment.sh
+++ k8sdeployment.sh
@@ -7,5 +7,6 @@ elif [ $deployname ==  products ];then
 deployappport=8082
 else
 deployappport=8080
+fi
 kubectl create deployment $1 --image=gcr.io/${GOOGLE_CLOUD_PROJECT}/$1:$2
 kubectl expose deployment $1 --type=LoadBalancer --port 80 --target-port $deployappport
